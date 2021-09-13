dfb44201493d1f9c967d8709f230a01f449f74d5
tphi10761 <tphi10761@gmail.com>
2021-09-13 10:19:30 +0700

    fix PORT

--- src/index.js
+++ src/index.js
@@ -22,6 +22,6 @@ app.set('views', path.join(__dirname, 'resource/views'));
 
 route(app);
 
-app.listen(port, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
